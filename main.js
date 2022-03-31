2c19ff7b5b3a2b56da4d8ba7f0e84833f1ec8bfa
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-31 15:22:32 +0100

    Bozza

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = new Vue(
+{
+    el: '#root',
+    data: {
+        immagini: [
+            'https://source.unsplash.com/random/?shoes',
+            'https://source.unsplash.com/random/?fruits',
+            'https://source.unsplash.com/random/?city',
+            'https://source.unsplash.com/random/?girls',
+          ]
+    //   image: 'https://source.unsplash.com/random'
+  
+  }
+}
+)
